2569ab17422893ee18dca0de858f417e39cf4d4a
katsuyanakata <kn19931118@gmail.com>
2026-03-02 01:05:27 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserOrOrgSite = repositoryName.toLowerCase().endsWith(".github.io");
+const hasRepositoryName = repositoryName.length > 0;
+const basePath = isGithubActions && hasRepositoryName && !isUserOrOrgSite ? `/${repositoryName}` : "";
+
+const nextConfig = {
+  output: isGithubActions ? "export" : undefined,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath || undefined
+};
 
 export default nextConfig;
